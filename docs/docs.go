db9b321a2867cf247879416bd3cffcdb7487c676
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-02-05 10:28:23 +0700

    docs: adding application documentation

--- docs/docs.go
+++ docs/docs.go
@@ -55,12 +55,12 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "",
+	Version:          "1.0.1",
 	Host:             "",
 	BasePath:         "",
 	Schemes:          []string{},
-	Title:            "",
-	Description:      "",
+	Title:            "Open Courseware Application",
+	Description:      "This is Open Couseware backend",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 }
