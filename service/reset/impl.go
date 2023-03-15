ddd13d150bc916bace4a69b8d9994bb674bc33f2
nart4hire <nart4hire@gmail.com>
2023-03-15 17:22:51 +0700

    fix(reset): removed unused dependencies

--- service/reset/impl.go
+++ service/reset/impl.go
@@ -5,7 +5,6 @@ import (
 	"gitlab.informatika.org/ocw/ocw-backend/repository/cache"
 	"gitlab.informatika.org/ocw/ocw-backend/repository/user"
 	"gitlab.informatika.org/ocw/ocw-backend/service/logger"
-	"gitlab.informatika.org/ocw/ocw-backend/service/verification"
 	"gitlab.informatika.org/ocw/ocw-backend/utils/env"
 	"gitlab.informatika.org/ocw/ocw-backend/utils/password"
 	"gitlab.informatika.org/ocw/ocw-backend/utils/template"
@@ -18,7 +17,6 @@ type ResetServiceImpl struct {
 	password.PasswordUtil
 	*env.Environment
 	token.TokenUtil
-	verification.VerificationService
 	logger.Logger
 	mail.MailQueue
 	template.TemplateWritterBuilder
