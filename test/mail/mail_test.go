693aa4469c75bf8872e138859f637b9de0ac2d12
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-04-28 16:21:32 +0700

    fix: ignoring test mail

--- test/mail/mail_test.go
+++ test/mail/mail_test.go
@@ -8,14 +8,14 @@ import (
 	"gitlab.informatika.org/ocw/ocw-backend/utils/env"
 )
 
-func TestSendMail(t *testing.T) {
+func IgnoreTestSendMail(t *testing.T) {
 	smtpClient := smtp.New(&env.Environment{
-		SmtpUsername: "postmaster@bayusamudra.my.id",
-		SmtpPassword: "bfbd251bf296826f4c103646b45e4b64-70c38fed-ed2d2e67",
+		SmtpUsername: "",
+		SmtpPassword: "",
 		SmtpIdentity: "",
-		SmtpServer:   "smtp.mailgun.org",
-		SmtpPort:     587,
-		SmtpAuthType: "plain",
+		SmtpServer:   "",
+		SmtpPort:     22,
+		SmtpAuthType: "",
 	})
 
 	err := smtpClient.Send([]string{"bayusamudra.55.02.com@gmail.com"}, "Testing", "Ini test")
